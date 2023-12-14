35d8fb114d085ae3f531be439fbb3af957642178
Rui Pereira <rui.goncalves@stud.edubs.ch>
2023-12-14 23:11:15 +0100

    added footer, fixed some stuff

--- script.js
+++ script.js
@@ -6,3 +6,12 @@ window.addEventListener('scroll', function() {
       navbar.classList.remove('transparent');
     }
   });
+
+  window.addEventListener('scroll', function() {
+    var footer = document.querySelector('footer'); // replace 'footer' with the selector for your footer
+    if ((window.innerHeight + window.scrollY) >= (document.body.offsetHeight - 50)) { // 50 is the threshold
+      footer.classList.add('transparent');
+    } else {
+      footer.classList.remove('transparent');
+    }
+  });
